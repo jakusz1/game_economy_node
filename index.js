3988386dfd008bbc942d99273476fb9b74fb6c9d
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2017-11-02 20:07:54 +0100

    connection with db fixed

--- index.js
+++ index.js
@@ -5,62 +5,92 @@ var mongo = require('mongodb').MongoClient;
 
 const START_BALANCE = 15000;
 
-mongo.connect('mongodb://127.0.0.1/test', function(err, db){
-	if(err) throw err;
 
-	io.sockets.on('connection', function (socket) {
-		console.log("Socket connected.");
-		var col = db.collection('users');
-
-		col.find().toArray(function(err, res){
-			if(err) throw err;
-			socket.emit('output', res);
-		});
-
-		socket.on('user joined', function (data) {
-            //if(!col.findOne({user: data.user})){
-                col.insertOne({user: data.user, balance: START_BALANCE});
-                socket.emit('new user',{user: data.user});
-            //}
+io.sockets.on('connection', function (socket) {
+    console.log("Socket connected.");
+    mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+        if (err) throw err;
+        var col = db.collection('users');
+        col.find().toArray(function (err, res) {
+            if (err) throw err;
+            socket.emit('output', res);
         });
+        db.close();
+    });
 
-		socket.on('balance change', function(data){
-            updateBalance(data.user,data.change,data.if_add);
-		});
-        socket.on('transfer', function(data){
-            updateBalance(data.user_minus,data.change,false);
-            updateBalance(data.user_plus,data.change,true);
+    socket.on('user joined', function (data) {
+        mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+            if (err) throw err;
+            var col = db.collection('users');
+            col.insertOne({user: data.user, balance: START_BALANCE}, function (err) {
+                if (!err) {
+                    io.emit('new user', {user: data.user});
+                }
+            });
+            db.close();
         });
-
-        function updateBalance(user,change,if_add) {
-            var whitespacePattern = /^\s*$/;
-
-            if(whitespacePattern.test(user) || whitespacePattern.test(change)) {
-                socket.emit('er', "Wartość i nazwa użytkownika nie może być pusta.");
-            }
-
-            else {
-                if (if_add) {
-                    col.updateOne({user: user}, {$inc: {balance: change}});
-                }
-                else{
-                    col.updateOne({user: user}, {$inc: {balance: -change}});
-                }
-                io.emit('to ' + user, {
-                    balance: col.findOne({user: user},{balance: 1})
+        mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+            if (err) throw err;
+            var col = db.collection('users');
+            col.findOne({user: data.user}, function (err, result) {
+                if (err) throw err;
+                socket.emit('update: ' + data.user, {
+                    balance: result.balance
                 });
+            });
+            db.close();
+        });
+    });
+
+    socket.on('balance change', function (data) {
+        updateBalance(data.user, data.change, data.if_add);
+    });
+    socket.on('transfer', function (data) {
+        updateBalance(data.user_minus, data.change, false);
+        updateBalance(data.user_plus, data.change, true);
+    });
+
+    function updateBalance(user, change, if_add) {
+        // var whitespacePattern = /^\s*$/;
+        //
+        // if (whitespacePattern.test(user) || whitespacePattern.test(change)) {
+        //     socket.emit('er', "Wartość i nazwa użytkownika nie może być pusta.");
+        // }
+        //
+        // else {
+        mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+            if (err) throw err;
+            var col = db.collection('users');
+            if (if_add) {
+                col.updateOne({user: user}, {$inc: {balance: change}});
             }
-        }
+            else {
+                col.updateOne({user: user}, {$inc: {balance: -change}});
+            }
+            db.close();
+        });
+        mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+            if (err) throw err;
+            var col = db.collection('users');
+            col.findOne({user: user}, function (err, result) {
+                if (err) throw err;
+                io.emit('update: ' + user, {
+                    balance: result.balance
+                });
+            });
+            db.close();
+        });
 
+        //}
+    }
 
-	});
 
 });
 
 
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 
@@ -70,6 +100,6 @@ app.get('/', function(req, res){
 // 	});
 // });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
