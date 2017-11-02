c55fa350227af11e1e91c4687111d5ca3dc2b031
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2017-11-02 14:49:15 +0100

    database usage done, todo display

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongo = require('mongodb').MongoClient;
 
-// var MongoClient = require('mongodb').MongoClient
-// , assert = require('assert');
+const START_BALANCE = 15000;
 
-// Connection URL
-
-// Use connect method to connect to the server
 mongo.connect('mongodb://127.0.0.1/test', function(err, db){
 	if(err) throw err;
 
@@ -20,54 +16,54 @@ mongo.connect('mongodb://127.0.0.1/test', function(err, db){
 			if(err) throw err;
 			socket.emit('output', res);
 		});
+		
+		socket.on('user joined', function (data) {
+            if(!col.find({user: data.user}).limit(1)){
+                col.insertOne({user: data.user, balance: START_BALANCE});
+                socket.emit('new user',{user: data.user});
+            }
+        });
 
-		socket.on('chat message', function(msg){
-
-			var whitespacePattern = /^\s*$/;
-
-			if(whitespacePattern.test(msg.user) || whitespacePattern.test(msg.message)) {
-				socket.emit('er', "Wiadomość i nazwa użytkownika nie może być pusta.");
-			}
-
-			else {
-				col.insert( { user: msg.user, message: msg.message} )
-				io.emit('chat message', {
-					message: msg.message,
-					user: msg.user
-				});
-			}
-
+		socket.on('balance change', function(data){
+            updateBalance(data.user,data.change,data.if_add);
 		});
-		socket.on('plus change', function(data){
+        socket.on('transfer', function(data){
+            updateBalance(data.user_minus,data.change,false);
+            updateBalance(data.user_plus,data.change,true);
+        });
 
-			var whitespacePattern = /^\s*$/;
+        function updateBalance(user,change,if_add) {
+            var whitespacePattern = /^\s*$/;
 
-			if(whitespacePattern.test(msg.user) || whitespacePattern.test(msg.message)) {
-				socket.emit('er', "Wiadomość i nazwa użytkownika nie może być pusta.");
-			}
+            if(whitespacePattern.test(user) || whitespacePattern.test(change)) {
+                socket.emit('er', "Wartość i nazwa użytkownika nie może być pusta.");
+            }
 
-			else {
-				if(col.find(data.user))
-				col.insert( { user: msg.user, message: msg.message} )
+            else {
+                if (if_add) {
+                    col.updateOne({user: data.user}, {$add: {balance: data.change}});
+                }
+                else{
+                    col.updateOne({user: data.user}, {$subtract: {balance: data.change}});
+                }
+                io.emit('to ' + data.user, {
+                    change: data.change,
+                    add: if_add
+                });
+            }
+        }
 
-				io.emit('to ' + data.user, {
-					change: data.change,
-					add: true
-				});
-			}
-
-		});
 
 	});
 
 });
 
+
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
-app.get('/screen', function(req, res){
-	res.sendFile(__dirname + '/screen.html');
-});
+
 
 // io.on('connection', function(socket){
 // 	socket.on('chat message', function(msg,user){
