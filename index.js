e51dc3256e7980d2bbbaf38e47e36d71751a9f53
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2017-11-04 21:53:25 +0100

    added server commands and fixed frontend style

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express'),app=express();
+var express = require('express'), app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongo = require('mongodb').MongoClient;
@@ -49,52 +49,55 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('balance change', function (data) {
-        updateBalance(data.user, data.change, data.if_add);
-        console.log(data.user+" "+(data.if_add?"+":"-")+data.change);
+        if (data.change !== null) {
+            updateBalance(data.user, data.change, data.if_add);
+            console.log(data.user + " " + (data.if_add ? "+" : "-") + data.change);
+        }
     });
     socket.on('transfer', function (data) {
-        updateBalance(data.user_minus, data.change, false);
-        updateBalance(data.user_plus, data.change, true);
-        console.log(data.user_minus+" ---"+data.change+"--> "+data.user_plus);
+        if (data.change !== null) {
+            updateBalance(data.user_minus, data.change, false);
+            updateBalance(data.user_plus, data.change, true);
+            console.log(data.user_minus + " ---" + data.change + "--> " + data.user_plus);
+        }
     });
 
     function updateBalance(user, change, if_add) {
 
-        if (change!==null) {
-            mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
-                if (err) throw err;
-                var col = db.collection('users');
-                if (if_add) {
-                    col.updateOne({user: user}, {$inc: {balance: change}});
-                }
-                else {
-                    col.updateOne({user: user}, {$inc: {balance: -change}});
-                }
-                db.close();
-            });
-            mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
-                if (err) throw err;
-                var col = db.collection('users');
-                col.findOne({user: user}, function (err, result) {
-                    if (err) throw err;
-                    io.emit('update: ' + user, {
-                        balance: result.balance
-                    });
-                });
-                db.close();
-            });
 
-        }
+        mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+            if (err) throw err;
+            var col = db.collection('users');
+            if (if_add) {
+                col.updateOne({user: user}, {$inc: {balance: change}});
+            }
+            else {
+                col.updateOne({user: user}, {$inc: {balance: -change}});
+            }
+            db.close();
+        });
+        mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+            if (err) throw err;
+            var col = db.collection('users');
+            col.findOne({user: user}, function (err, result) {
+                if (err) throw err;
+                io.emit('update: ' + user, {
+                    balance: result.balance
+                });
+            });
+            db.close();
+        });
+
     }
 
 
-});
+})
+;
 
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
 
 
 // io.on('connection', function(socket){
@@ -106,3 +109,58 @@ app.get('/', function (req, res) {
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (text) {
+    var words = text.match(/[^\s]+/g);
+    switch(words[0]) {
+        case 'exit':
+            done();
+            break;
+        case 'drop':
+            drop();
+            break;
+        case 'remove':
+            if(words[1]!==null){remove(words[1]);}
+            break;
+        default:
+    }
+});
+
+function done() {
+    console.log('>bye');
+    process.exit();
+}
+function drop() {
+    mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+        if (err) throw err;
+        var col = db.collection('users');
+        col.drop(function (err) {
+            if (err) throw err;
+        });
+
+    });
+    mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+        if (err) throw err;
+        var col = db.collection('users');
+        col.createIndex({user: 1},{unique: true}, function (err) {
+            if (err) throw err;
+        });
+        db.close();
+    });
+    console.log(">all users removed");
+}
+function remove(user) {
+    mongo.connect('mongodb://127.0.0.1/test', function (err, db) {
+        if (err) throw err;
+        var col = db.collection('users');
+        col.removeOne({user: user}, function (err) {
+            if (err) throw err;
+            console.log(">"+user+" was removed");
+        });
+        db.close();
+    });
+}
